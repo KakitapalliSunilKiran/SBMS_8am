4e86c693ab7006fc150fc82ab11a23aa111998ef
kakitapallisunilkiran <k.sunilkiran98@gmail.com>
2026-07-15 08:57:41 +0530

    15/07

---
+++ ThreetierArchitecture_MySQL/src/main/java/com/example/demo/ThreetierArchitectureRepoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.BusineesLayer;
+
+@SpringBootApplication
+public class ThreetierArchitectureRepoApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(ThreetierArchitectureRepoApplication.class, args);
+//		    RepoLayer rl = ctx.getBean(RepoLayer.class);
+//		    
+////		    Parent p = new Child();
+//		    System.out.println(rl.getClass());
+		
+//		BusineesLayer bl = ctx.getBean(BusineesLayer.class);
+//		Student obj = new Student(100,"Sunil","VIT UNiversity",80);
+//		bl.saveData(obj);
+	}
+
+}
