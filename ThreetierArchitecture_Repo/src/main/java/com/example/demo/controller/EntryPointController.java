98e19b572e58f49e7c08473d5ff0455729ee2062
kakitapallisunilkiran <k.sunilkiran98@gmail.com>
2026-07-16 09:08:00 +0530

    16/07

--- ThreetierArchitecture_Repo/src/main/java/com/example/demo/controller/EntryPointController.java
+++ ThreetierArchitecture_Repo/src/main/java/com/example/demo/controller/EntryPointController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,24 +25,42 @@ public class EntryPointController {
 	BusineesLayer bl;
 	
 	@PostMapping("/save")
-	public void doSave(@RequestBody Student s) {
-		bl.saveData(s);
+	public ResponseEntity<String> doSave(@RequestBody Student s) {
+	    bl.saveData(s);
+	    return ResponseEntity.status(HttpStatus.CREATED)
+	                         .body("Student saved successfully");
 	}
 	
 	@GetMapping("/fetch/{id}")
-	public Optional<Student> getData(@PathVariable("id") Integer id){
-		return bl.getData(id);
+	public ResponseEntity<Student> getData(@PathVariable("id") Integer id) {
+
+	    Optional<Student> student = bl.getData(id);
+
+	    if (student.isPresent()) {
+	        return ResponseEntity.ok(student.get());
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
 	}
 	//localhost:8080/api/v1/fetch/100
 	
 	@PutMapping("/update/{id}")
-	public Student updateData(@PathVariable("id") Integer id) {
-		return bl.updateData(id);
+	public ResponseEntity<Student> updateData(@PathVariable Integer id) {
+
+	    Student student = bl.updateData(id);
+	    if (student != null) {
+	        return ResponseEntity.ok(student);
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
 	}
 	
 	@DeleteMapping("/delete/{id}")
-	public void deleteData(@PathVariable("id") Integer id) {
-		 bl.deleteData(id);
+	public ResponseEntity<Void> deleteData(@PathVariable Integer id) {
+
+	    bl.deleteData(id);
+
+	    return ResponseEntity.noContent().build();
 	}
 	
 	//Data Jpa
